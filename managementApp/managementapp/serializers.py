d5b81ad34def60b1fcc3c74491d212d973be8816
wanis ramdani <wanisramdani00@gmail.com>
2021-09-26 10:29:34 +0100

    Expose Id field

--- managementApp/managementapp/serializers.py
+++ managementApp/managementapp/serializers.py
@@ -5,24 +5,24 @@ from .models import Client, SubTask, Task, Project
 class ClientSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Client
-        fields = ['name', 'email', 'phoneNumber', 'address']
+        fields = ['id' ,'name', 'email', 'phoneNumber', 'address']
 
 
 class SubTaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = SubTask
-        fields = ['title', 'priority', 'deadline', 'daysLeft', 'status', 'responsible_client']
+        fields = ['id' ,'title', 'priority', 'deadline', 'daysLeft', 'status', 'responsible_client']
 
 
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Task
-        fields = ['title', 'priority', 'deadline', 'daysLeft', 'status', 'responsible_client', 'subTasks']
+        fields = ['id' ,'title', 'priority', 'deadline', 'daysLeft', 'status', 'responsible_client', 'subTasks']
         
 
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Project
-        fields = ['title', 'priority', 'deadline', 'daysLeft', 'status', 'responsible_client', 'tasks']
+        fields = ['id' ,'title', 'priority', 'deadline', 'daysLeft', 'status', 'responsible_client', 'tasks']
